e46b7d421d735160f3ffe79ec61767930ff5f265
Manu <manuel.garcia@gnommo.com>
2016-12-26 15:46:16 +0100

    Added gitignore

--- build/sources/main/java/com/manu/mim/Mim.java
+++ build/sources/main/java/com/manu/mim/Mim.java
@@ -4,11 +4,12 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Mim.MODID, version = Mim.VERSION)
 public class Mim
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mim";
     public static final String VERSION = "1.0";
     
     @EventHandler
@@ -16,5 +17,11 @@ public class Mim
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        System.out.println("Manu");
     }
 }
