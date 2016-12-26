bca97521ebb81d0bb4b97543cfca11eb4b8753b9
Manu <manuel.garcia@gnommo.com>
2016-12-26 15:36:18 +0100

    first commit

---
+++ build/sources/main/java/com/manu/mim/Mim.java
@@ -0,0 +1,20 @@
+package com.manu.mim;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Mim.MODID, version = Mim.VERSION)
+public class Mim
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
---
+++ src/main/java/com/manu/mim/Mim.java
@@ -0,0 +1,27 @@
+package com.manu.mim;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Mim.MODID, version = Mim.VERSION)
+public class Mim
+{
+    public static final String MODID = "mim";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        System.out.println("Manu");
+    }
+}
